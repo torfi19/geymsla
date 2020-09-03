e8848bb0b0a41047c562514d337b0d18a89d1cd8
torfi19 <torfi19@ru.is>
2020-09-03 14:07:21 +0000

    second commit með koða

--- max_int.py
+++ max_int.py
@@ -1,2 +1,13 @@
-Þessi algorithmi finnu maximum positve integer input frá usernum. 
-Userinn setur inn tölu þar til neikvæð tala er sett inn
+#Þessi algorithmi finnu maximum positve integer input frá usernum. 
+#Userinn setur inn tölu þar til neikvæð tala er sett inn
+
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+max_int=0
+while num_int>0 :
+    if num_int>max_int :
+        max_int=num_int
+    num_int = int(input("Input a number: "))
+       
+print("The maximum is", max_int)    # Do not change this line
